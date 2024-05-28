2742a61033bb0d785fae2f3525fe4a756f2909a3
Hansen <hansen.zheng@hotmail.com>
2024-05-28 17:34:06 +0800

    update formatter

--- lua/lazynvim-init.lua
+++ lua/lazynvim-init.lua
@@ -148,6 +148,44 @@ local toggleterm = {
     config = true
 }
 
+local conform = {
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  keys = {
+    {
+      -- Customize or remove this keymap to your liking
+      "<leader>fo",
+      function()
+        require("conform").format({ async = true, lsp_fallback = true })
+      end,
+      mode = "",
+      desc = "Format buffer",
+    },
+  },
+  -- Everything in opts will be passed to setup()
+  opts = {
+    -- Define your formatters
+    formatters_by_ft = {
+      lua = { "stylua" },
+      python = { "isort" },
+      javascript = { "prettierd" },
+    },
+    -- Set up format-on-save
+    format_on_save = { timeout_ms = 500, lsp_fallback = true },
+    -- Customize formatters
+    formatters = {
+      shfmt = {
+        prepend_args = { "-i", "2" },
+      },
+    },
+  },
+  init = function()
+    -- If you want the formatexpr, here is the place to set it
+    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+  end,
+}
+
 -- 3. 加载lazy.nvim模块
 require("lazy").setup({
     nvim_tree_plugin, 
@@ -159,6 +197,7 @@ require("lazy").setup({
     tree_sitter,
     nvim_cmp,
     codeium,
-    toggleterm
+    toggleterm,
+    conform
 })
    
